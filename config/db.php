300b7346ad34c1e677c5c4a8377aead0c21864bb
chasaurav <59362110+chasaurav@users.noreply.github.com>
2019-12-30 17:37:19 +0530

    Update db.php

--- config/db.php
+++ config/db.php
@@ -1,8 +1,8 @@
 <?php
-$dbname = 'interview';
-$dbhost = 'localhost';
-$password = '';
-$username = 'root';
+$dbname = 'vpejypg1a3r2zoja';
+$dbhost = 'sp6xl8zoyvbumaa2.cbetxkdyhwsb.us-east-1.rds.amazonaws.com';
+$username = 'sm1qc9xvhns8jr9y';
+$password = 'y6xsa57ui47c9xn7';
 $dsn = "mysql:dbname=$dbname;host=$dbhost";
 $pdo = new PDO($dsn, $username, $password);
 $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_WARNING);
